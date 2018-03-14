6927892589a07524a2ee0643490ae4c89cb81373
Zack Creach <zackcreach@gmail.com>
2018-03-13 22:39:46 -0400

    Updated naming.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
+import Header from '../components/header'
 
 import { css } from 'react-emotion'
 import '../styles/styles.scss'
